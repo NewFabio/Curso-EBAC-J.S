407192f4c27ee050d390ed70a905850cc7f81d83
Fabio Conceição <fabio.conceicaoads@gmail.com>
2024-07-26 13:46:07 -0300

    aula 26

---
+++ 7_DOM_seletores_formularios_Estilizacao/script_Estilizacao.js
@@ -0,0 +1,6 @@
+function aplicarEstilos(){
+    elementoParaEstilizar.style.backgroundColor = "red";
+    elementoParaEstilizar.style.cor = "blue";
+    elementoParaEstilizar.style.fontWeight = "bold";
+    elementoParaEstilizar.style.borderRadius = "10px";
+}

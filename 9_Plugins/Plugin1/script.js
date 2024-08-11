f333ecbd96cea2e8e4212ab312e97eb44caeeeea
NewFabio <fabio.conceicaoads@gmail.com>
2024-08-10 21:31:29 -0300

    aula portifolio

---
+++ 9_Plugins/Plugin1/script.js
@@ -0,0 +1,8 @@
+document.getElementById('alertButton').addEventListener('click', function() {
+    swal({
+        title: "Boa!",
+        text: "Voce clicou no botão!",
+        icon: "Success",
+        button: "Fechar",
+    });
+});
